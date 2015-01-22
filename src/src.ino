fbb08f9f81fbd1632715cbd24b1c779f876ae0ec
Rob Mac <maccelerated@gmail.com>
2015-01-22 00:22:02 -0800

    Convert TCP to UDP communications
    UDP packets are WAY more promising and allow us to broadcast and track
    "connected" clients by IP and port. UDP allows some packets to be
    dropped, but the ROV could simply know when the last command was
    received and work from there.

--- src/src.ino
+++ src/src.ino
@@ -1,8 +1,6 @@
 #include <SPI.h>
 #include <Ethernet.h>
-
-#include <SoftwareSerial.h>
-#include <ParallaxLCD.h>
+#include <EthernetUdp.h>
 
 /* ============================================================================
 	Settings
@@ -10,15 +8,13 @@
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-const char* ip_to_str(const uint8_t*);
+IPAddress ip		(10,1,1,1);
+IPAddress gateway	(10,0,0,1);
+IPAddress subnet	(255,0,0,0);
 
-IPAddress ip		(192,168,1,177);
-IPAddress gateway	(192,168,1,1);
-IPAddress subnet	(255,255,0,0);
+unsigned int localPort = 8888;
 
-EthernetServer server(80); // port
-
-ParallaxLCD lcd(7, 2, 16, 9600); //  pin, rows, cols, baud
+EthernetUDP Udp;
 
 /* ============================================================================
 	Main
@@ -26,32 +22,75 @@ ParallaxLCD lcd(7, 2, 16, 9600); //  pin, rows, cols, baud
 
 void setup()
 {
-	lcd.setup();
-	delay(1000); // Wait for the LCD to start up
-	lcd.backLightOn();
-
-	lcd.at(0,0, "Waiting for DHCP lease...");
+	startDebugSerial();
 
 	if (Ethernet.begin(mac) == 0) {
-		lcd.empty();
-		lcd.at(0,0, "Static IP");
+		// DHCP will timeout after 1 minute.
 		Ethernet.begin(mac, ip, gateway, subnet);
-	} else {
-		lcd.empty();
-		lcd.at(0,0, "DHCP IP");
 	}
+	Serial.print("IP: ");
+	Serial.println(Ethernet.localIP());
 
-	lcd.at(1,0, ip_to_str(Ethernet.localIP()));
-	server.begin();
+	Udp.begin(localPort);
 }
 
 void loop()
 {
-
+	handleIncomingData();
+	broadcast(4000);
+	delay(10);
 }
 
 /* ============================================================================
-	Networking Basics
+	Networking
+============================================================================ */
+
+void handleIncomingData ()
+{
+	static char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+	static char replyBuffer[] = "acknowledged";
+
+	int packetSize = Udp.parsePacket();
+
+	if (packetSize) {
+
+		Serial.print("Received packet of size ");
+		Serial.println(packetSize);
+		Serial.print("From ");
+		Serial.print(ip_to_str(Udp.remoteIP()));
+		Serial.print(", port ");
+		Serial.println(Udp.remotePort());
+
+		// read the packet into packetBufffer
+		Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+		Serial.println("Contents:");
+		Serial.println(packetBuffer);
+
+		// send a reply, to the IP address and port that sent us the packet we received
+		Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+		Udp.write(replyBuffer);
+		Udp.endPacket();
+	}
+}
+
+void broadcast (int gap) {
+
+	static unsigned long last = -gap;
+	static const IPAddress BROADCAST_IP (255,255,255,255);
+	static const int BROADCAST_PORT (8888);
+
+	if (millis() - last > gap) {
+		last = millis();
+		Serial.println("Broadcast!");
+		Serial.print("IP: ");
+		Serial.println(ip_to_str(BROADCAST_IP));
+		Serial.print("PORT: ");
+		Serial.println(BROADCAST_PORT, DEC);
+	}
+}
+
+/* ============================================================================
+	Debugging
 ============================================================================ */
 
 void startDebugSerial ()
@@ -60,14 +99,7 @@ void startDebugSerial ()
 	// This loop is for compatibility with Leonardo.
 	// Other models will still work fine too.
 	while (!Serial) { }
-}
-
-
-const char* ip_to_str(const uint8_t* ipAddr)
-{
-	static char buf[16];
-	sprintf(buf, "%d.%d.%d.%d\0", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
-	return buf;
+	Serial.println("Serial logging started.");
 }
 
 const char* ip_to_str(const IPAddress ipAddr)
@@ -75,65 +107,4 @@ const char* ip_to_str(const IPAddress ipAddr)
 	static char buf[16];
 	sprintf(buf, "%d.%d.%d.%d\0", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
 	return buf;
-}
-
-
-
-
-
-
-
-
-void handleClient ()
-{
-// listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-	Serial.println("new client");
-	// an http request ends with a blank line
-	boolean currentLineIsBlank = true;
-	while (client.connected()) {
-	  if (client.available()) {
-		char c = client.read();
-		Serial.write(c);
-		// if you've gotten to the end of the line (received a newline
-		// character) and the line is blank, the http request has ended,
-		// so you can send a reply
-		if (c == '\n' && currentLineIsBlank) {
-		  // send a standard http response header
-		  client.println("HTTP/1.1 200 OK");
-		  client.println("Content-Type: text/html");
-		  client.println("Connection: close");  // the connection will be closed after completion of the response
-	  client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-		  client.println();
-		  client.println("<!DOCTYPE HTML>");
-		  client.println("<html>");
-		  // output the value of each analog input pin
-		  for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-			int sensorReading = analogRead(analogChannel);
-			client.print("analog input ");
-			client.print(analogChannel);
-			client.print(" is ");
-			client.print(sensorReading);
-			client.println("<br />");
-		  }
-		  client.println("</html>");
-		  break;
-		}
-		if (c == '\n') {
-			// you're starting a new line
-			currentLineIsBlank = true;
-		}
-		else if (c != '\r') {
-			// you've gotten a character on the current line
-			currentLineIsBlank = false;
-		}
-	  }
-	}
-	// give the web browser time to receive the data
-	delay(1);
-	// close the connection:
-	client.stop();
-	Serial.println("client disonnected");
-  }
 }
